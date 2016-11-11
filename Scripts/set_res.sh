3dcc2376c95c57bde5b284a8aa3feda11bf35ba4
Roy Moore <roy@moore.co.il>
2016-11-11 20:25:49 +0200

    fixed set_res size calc

---
+++ Scripts/set_res.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# setup the current resolution in the config file
+
+dims=$(xdpyinfo  | grep 'dimensions:' | awk '{print $2}' | egrep -o '[0-9]+')
+width=$(echo $dims | awk '{print $1}')
+height=$(echo $dims | awk '{print $2}')
+minimum_width=$((${width}-200))
+minimum_height=$((${height}-200))
+
+new_line="	minimum_width = $minimum_width, minimum_height = $minimum_height,"
+old_line="	minimum_width = [0-9]*, minimum_height = [0-9]*,"
+
+sed -i "s/${old_line}/${new_line}/" main
